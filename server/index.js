31243dc5335c5958e471e0b740113f0c167f6656
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-25 02:36:32 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const cors = require("cors")
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const cookieParser = require("cookie-parser")
+// get usermodel
+const UserModel = require("./Models/UserModel")
+
 
 const app = express()
 app.use(express.json())
@@ -12,6 +15,17 @@ app.use(cookieParser)
 
 //make connection between database
 mongoos.connect('mongodb://127.0.0.1:27017/employee')
+
+app.post('/register', (req, res) => {
+    const {name, email, passord} = req.body;
+    bcrypt.hash(password, 10)
+    .then(hash => {
+        UserModel.create({name, email, password: hash})
+        .then(user => res.json({status: "OK"}))
+        .catch(err => res.json(err))
+    }).catch(err => res.json(err))
+})
+
 
 // run the app
 app.listen(3001, () => {
