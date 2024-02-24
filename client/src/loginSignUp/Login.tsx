f0030df3c29f2c9a1647f7253fce816e519542ad
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-25 02:15:16 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/loginSignUp/Login.tsx
+++ client/src/loginSignUp/Login.tsx
@@ -5,6 +5,14 @@ const Login = () => {
             <div className="bg-white py-12 px-16 rounded">
                 <h1 className="font-semibold text-3xl">Login Here</h1>
                 <hr className="mb-4"/>
+
+                <form>
+                    <div className="">
+                        <label htmlFor="">Name</label>
+                        <input type="text" className="w-full h-8 border" />
+                    </div>
+                </form>
+
             </div>
         </div>
     </div>
