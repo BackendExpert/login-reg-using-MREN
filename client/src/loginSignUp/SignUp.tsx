2d52c1d907b0a1bb330bd71451984f0ecec69394
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-25 02:44:24 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/loginSignUp/SignUp.tsx
+++ client/src/loginSignUp/SignUp.tsx
@@ -1,9 +1,19 @@
+import axios from "axios"
 import { useState } from "react"
 
 const SignUp = () => {
     const [name, SetName] = useState("")
     const [email, SetEmail] = useState("")
     const [password, SetPassword] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:3001/register', {name, email, password})
+        .then(res => {
+            alert("Created")
+        }).catch(err => console.log(err))
+    }
+
   return (
     <div className="bg-blue-600 w-full h-screen">
         <div className="container mx-auto px-40 py-20">
@@ -11,7 +21,7 @@ const SignUp = () => {
                 <h1 className="font-semibold text-3xl">SigmUp Here</h1>
                 <hr className="mb-4"/>
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="mt-4">
                         <label htmlFor="text-xl pb-8">Name</label>
                         <input type="text" className="w-full h-12 border rounded pl-4" placeholder="Name"
