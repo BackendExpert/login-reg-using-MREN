d0c1490d2a5c4bcf8506b3a0a26bd94f009d171b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-25 02:40:47 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/loginSignUp/SignUp.tsx
+++ client/src/loginSignUp/SignUp.tsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 const SignUp = () => {
     const [name, SetName] = useState("")
     const [email, SetEmail] = useState("")
-    const [password, SetPassword] = useS
+    const [password, SetPassword] = useState("")
   return (
     <div className="bg-blue-600 w-full h-screen">
         <div className="container mx-auto px-40 py-20">
@@ -14,15 +14,18 @@ const SignUp = () => {
                 <form>
                     <div className="mt-4">
                         <label htmlFor="text-xl pb-8">Name</label>
-                        <input type="text" className="w-full h-12 border rounded pl-4" placeholder="Name"/>
+                        <input type="text" className="w-full h-12 border rounded pl-4" placeholder="Name"
+                        onChange={(e) => SetName(e.target.value)}/>
                     </div>
                     <div className="mt-4">
                         <label htmlFor="text-xl pb-8">Email</label>
-                        <input type="email" className="w-full h-12 border rounded pl-4" placeholder="Email"/>
+                        <input type="email" className="w-full h-12 border rounded pl-4" placeholder="Email" 
+                        onChange={(e) => SetEmail(e.target.value)}/>
                     </div>
                     <div className="mt-4">
                         <label htmlFor="text-xl pb-8">Password</label>
-                        <input type="password" className="w-full h-12 border rounded pl-4" placeholder="Password"/>
+                        <input type="password" className="w-full h-12 border rounded pl-4" placeholder="Password" 
+                        onChange={(e) => SetPassword(e.target.value)}/>
                     </div>
                     <div className="mt-4">
                         <button type="submit" className="w-full h-16 bg-green-600 text-xl text-white rounded duration-500 hover:bg-green-700">Sign Up</button>
